9b4a68f1f05cf1ed75c7a3d6ee7bcd79464d9e7e
DivyaKalyan24 <101103976+DivyaKalyan24@users.noreply.github.com>
2025-01-08 00:43:19 +0530

    .

--- server.py
+++ server.py
@@ -6,6 +6,10 @@ from predictor import predictor
 app = Flask(__name__)
 CORS(app, origins='https://vahann-value.vercel.app', methods=['POST'], supports_credentials=True)
 
+@app.route('/', methods=['GET'])
+def home():
+    return 'Hello World'
+    
 @app.route('/fetch-cars', methods=['POST'])
 def fetch_data():
     data = request.json
